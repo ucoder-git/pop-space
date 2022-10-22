f9804c8d07d082d73b5bb7154160cb7813c55c3b
ucoder-git <ucoder.git@gmail.com>
2022-10-23 06:05:20 +0900

    리액트 팝스페이스 쇼핑몰

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import ChildComponent from './child.js';
 
 function App() {
+  const text = "팝샵 여러분 화이팅!";
+  const sayHello = function(){
+    return <h3>이 인테리어 너무 좋아 </h3>
+  };
+  const sayHello2 = function(){
+    alert('안녕하세요');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div> 
+    <h1>안녕하세요!</h1> 
+    <h2>{text}</h2>
+    {sayHello()}
+    <div onClick={sayHello2}>클릭해주세요</div>
+    <ChildComponent name="팝샵" age={30}/>
+    <ChildComponent name="스페이스" age={20}/>
+    <ChildComponent name="황샘" age={40}/>
+    <ChildComponent name="정샘" age={30}/>
     </div>
   );
 }
